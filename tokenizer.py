4b6cc048375ad005ba60f633fffc3db682620f1b
Jigyasa Yadav <40534001+jigyasa97@users.noreply.github.com>
2019-04-14 16:58:36 +0530

    Update tokenizer.py

--- tokenizer.py
+++ tokenizer.py
@@ -5,11 +5,46 @@ Created on Sun Apr 14 16:40:51 2019
 @author: Jigyasa Yadav
 """
 from nltk.tokenize import sent_tokenize, word_tokenize
-example_text = "Hello mR. Sharma, how are you doing today? The weather is great and pythin is best.The sky is the limit.And now i should stop typing."
+example_text = "Hello mR. Sharma, how are you doing today? The weather is great and pythin is best.The sky is the limit. And now i should stop typing."
 print(sent_tokenize(example_text))
 print(word_tokenize(example_text))
 
 '''
-['Hello mR. Sharma, how are you doing today?', 'The weather is great and pythin is best.The sky is the limit.And now i should stop typing.']
-['Hello', 'mR.', 'Sharma', ',', 'how', 'are', 'you', 'doing', 'today', '?', 'The', 'weather', 'is', 'great', 'and', 'pythin', 'is', 'best.The', 'sky', 'is', 'the', 'limit.And', 'now', 'i', 'should', 'stop', 'typing', '.']
+['Hello mR. Sharma, how are you doing today?', 'The weather is great and pythin is best.The sky is the limit.', 'And now i should stop typing.']
+['Hello', 'mR.', 'Sharma', ',', 'how', 'are', 'you', 'doing', 'today', '?', 'The', 'weather', 'is', 'great', 'and', 'pythin', 'is', 'best.The', 'sky', 'is', 'the', 'limit', '.', 'And', 'now', 'i', 'should', 'stop', 'typing', '.']
+'''
+
+for i in word_tokenize(example_text):
+  print(i)
+  '''
+  Hello
+mR.
+Sharma
+,
+how
+are
+you
+doing
+today
+?
+The
+weather
+is
+great
+and
+pythin
+is
+best.The
+sky
+is
+the
+limit
+.
+And
+now
+i
+should
+stop
+typing
+.
 '''
